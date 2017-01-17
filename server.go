602caf79dee43daab6c550e746f95ae340584167
jiemicc <jiemicc@126.com>
2017-01-17 20:28:39 +0800

    add concurrency

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"log"
 	"net"
 	"net/http"
@@ -10,41 +11,51 @@ import (
 
 func main() {
 	// 1. Listen for connections forever.
-	if ln, err := net.Listen("tcp", ":8080"); err == nil {
-		for {
+	ln, err := net.Listen("tcp", ":8080")
+	// add error checking
+	if err != nil {
+		log.Fatalf("Failed to listen: %s", err)
+		return
+	}
+	for {
 
-			//2. Accept connections.
-			if conn, err := ln.Accept(); err == nil {
+		//2. Accept connections.
+		if conn, err := ln.Accept(); err == nil {
 
-				reader := bufio.NewReader(conn)
+			go handleConnection(conn)
 
-				//3. Read requests from the client.
-				if req, err := http.ReadRequest(reader); err == nil {
+		}
+	}
 
-					if be, err := net.Dial("tcp", "127.0.0.1:8081"); err == nil {
-						be_reader := bufio.NewReader(be)
+}
 
-						if err := req.Write(be); err == nil {
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
 
-							//6. Read the response from the backend
-							if resp, err := http.ReadResponse(be_reader, req); err == nil {
+	for {
+		req, err := http.ReadRequest(reader)
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("Failed to read request: %s", err)
+			}
+			return
+		}
 
-								//7. Send the response to the client.
-								resp.Close = true
-								if err := resp.Write(conn); err == nil {
-									log.Printf("%s: %d", req.URL.Path, resp.StatusCode)
-								}
-								conn.Close()
-								//Repeat back at 2: accept the next connection.
+		if be, err := net.Dial("tcp", "127.0.0.1:8081"); err == nil {
+			be_reader := bufio.NewReader(be)
 
-							}
-						}
+			if err := req.Write(be); err == nil {
+
+				//6. Read the response from the backend
+				if resp, err := http.ReadResponse(be_reader, req); err == nil {
+
+					if err := resp.Write(conn); err == nil {
+						log.Printf("%s: %d", req.URL.Path, resp.StatusCode)
 					}
 
 				}
-
 			}
 		}
-
 	}
 }
